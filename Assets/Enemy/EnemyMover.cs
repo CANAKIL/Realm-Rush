b9bf015327e3d3e19d54b7bc56d4718f67492d60
Can <canakil@hotmail.com>
2023-04-12 01:33:36 +0300

    added a currency system

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,58 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] private List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
+
+    Enemy enemy;
+
+    void OnEnable()
+    {
+        FindPath();
+        ReturnToStart();
+        StartCoroutine(FollowPath());
+    }
 
     void Start()
     {
-        PrintWaypointName();
+        enemy = GetComponent<Enemy>();
     }
 
-    void PrintWaypointName()
+    void FindPath()
+    {
+        path.Clear();
+        
+        GameObject[] waypoints = GameObject.FindGameObjectsWithTag("Path");
+
+        foreach (GameObject waypoint in waypoints)
+        {
+            path.Add(waypoint.GetComponent<Waypoint>());
+        }
+    }
+
+    void ReturnToStart()
+    {
+        transform.position = path[0].transform.position;
+    }
+    
+    IEnumerator FollowPath()
     {
         foreach (Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0;
+            
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
+        
+        enemy.StealGold();
+        gameObject.SetActive(false);
     }
 }
--- Assets/Tiles/Waypoint.cs
+++ Assets/Tiles/Waypoint.cs
@@ -1,18 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Tower towerPrefab;
+    
+    [SerializeField] bool isPlaceable;
+    public bool IsPlaceable { get { return isPlaceable; } }
+    
+    void OnMouseDown()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (isPlaceable)
+        {
+            bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
+            isPlaceable = !isPlaced;
+        }
     }
 }
